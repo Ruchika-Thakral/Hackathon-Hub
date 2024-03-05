3c28d974c7d28930a044ff9a3aa38b629557ddca
Ankit Bhanja <ankitbhanja99@gmail.com>
2024-03-05 15:36:13 +0530

    intial setup cra, material-tailwind

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { Button } from '@material-tailwind/react';
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="App">
+      <Button>Hello</Button>
+    </div>
+  );
+}
+
+export default App;
