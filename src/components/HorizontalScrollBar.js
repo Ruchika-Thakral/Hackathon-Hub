fdbdb9d56c4d91b2dd2f8d6901c873bf80375b44
Bhuvaneshwar <vajralabhuvaneshwarreddy@gmail.com>
2024-03-12 16:31:49 +0530

    created landing page,hackathons and results

---
+++ src/components/HorizontalScrollBar.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { Typography } from '@material-tailwind/react'
+import { Link } from 'react-router-dom'
+import styles from './HorizontalScrollBar.module.css'
+import { useContext } from 'react'
+import { CreateContext } from '../App'
+
+function HorizontalScrollBar() {
+    const {arr,setDetails}=useContext(CreateContext)
+    const clickHandler=(item)=>{
+        setDetails(item)
+    }
+
+  return (
+    <div className={`${styles.main} flex gap-x-4  w-9/12 overflow-x-auto  border border-black rounded-3xl p-4 mx-auto mt-4`} >
+    {
+    arr.map(item =>
+        <Link to='hackathons' key={item.id} onClick={()=>clickHandler(item)}>
+          <div className='shrink-0 w-96  h-48 border border-black rounded-3xl'>
+            <Typography  className='w-fit mx-auto'variant='h2' color='black'>{item.name}</Typography>
+            <Typography variant='h6' color='black'>Start Date:{item.start}</Typography>
+            <Typography variant='h6' color='black'>End Date:{item.end}</Typography>
+          </div>
+        </Link>
+    
+    )
+}
+</div>
+  )
+}
+
+export default HorizontalScrollBar
---
+++ src/components/Host.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Card, CardBody,Input,Typography,Button } from '@material-tailwind/react';
+
+const DualFormCard=()=> {
+  return (
+    <Card className='border border-black mt-4 mx-4'>
+      <CardBody className="flex">
+        {/* Left half with small form */}
+        <div className="w-1/2 p-4">
+          <Card>
+            <CardBody>
+              {/* <form>
+               
+                <input type="text" placeholder="Small Form Field 1" className="mb-4 w-full p-2 rounded border border-gray-300" />
+                <input type="text" placeholder="Small Form Field 2" className="mb-4 w-full p-2 rounded border border-gray-300" />
+              </form> */}
+            </CardBody>
+          </Card>
+        </div>
+
+        {/* Right half with larger form */}
+        <div className="w-1/2 p-4 ">
+          <Card>
+          <Typography variant="h2" color="blue-gray" className="mb-2 font-medium flex justify-center">
+                    Host your hackathon
+                </Typography>
+            <CardBody>
+              <form className='flex flex-col gap-y-2'>
+                {/* Larger form content */}
+                {/* For example: */}
+                <Input type="text" label='Name' placeholder="Enter you name" className="mb-4 w-full p-2 rounded border border-gray-300" />
+                <Input type="email" label='E-Mail' placeholder="Enter your E-mail" className="mb-4 w-full p-2 rounded border border-gray-300" />
+                <Input type="text" label='Details' placeholder="Enter details on the hackathon" className="mb-4 w-full p-2 rounded border border-gray-300" />
+                <Button className='btn-submit-form w-fit' type='submit' style={{ cursor: 'pointer' }}>
+                Submit
+            </Button>
+              </form>
+            </CardBody>
+          </Card>
+        </div>
+      </CardBody>
+    </Card>
+  );
+}
+
+export default DualFormCard;
