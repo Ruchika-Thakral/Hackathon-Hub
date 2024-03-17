b45bb98464afbc3a39b8b661341720f1bd258ba4
SingamsettiGokul <singamsettigokul@gmail.com>
2024-03-17 16:10:58 +0530

    new changes

--- src/main/java/com/example/capstone/Controller/PanelistController.java
+++ src/main/java/com/example/capstone/Controller/PanelistController.java
@@ -7,12 +7,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.capstone.DTO.TeamDetailsToPanelistDTO;
 import com.example.capstone.Service.PanelistService;
 
 @RestController
+@RequestMapping("panelist")
 public class PanelistController {
 	@Autowired
 	private PanelistService panelistService;
