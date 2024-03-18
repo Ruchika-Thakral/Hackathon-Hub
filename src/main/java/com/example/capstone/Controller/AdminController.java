2f5221da1938d1c152ac7dee27f0e2e25b6a8800
Sharath198118 <103941248+Sharath198118@users.noreply.github.com>
2024-03-18 15:36:08 +0530

    new changes

--- src/main/java/com/example/capstone/Controller/AdminController.java
+++ src/main/java/com/example/capstone/Controller/AdminController.java
@@ -20,41 +20,51 @@ import com.example.capstone.DTO.GetEvaluatorsDTO;
 import com.example.capstone.DTO.HackathonDTO;
 import com.example.capstone.Service.AdminService;
 
+
+//Controller for admin related API endpoints
 @RestController
 @RequestMapping("Admin")
 public class AdminController {
 
+	// Injecting the AdminService dependency
 	@Autowired
 	private AdminService adminService;
 
+	// Endpoint to create a new hackathon
 	@PostMapping("hackathon")
 	public ResponseEntity<String> createHackathon(@RequestBody CreateHackathonDTO createHackathonDTO) {
 		adminService.createHackathon(createHackathonDTO);
 		return ResponseEntity.status(HttpStatus.CREATED).body("Hackathon created successfully");
 		}
 
+	// Endpoint to register a new evaluator
 	@PostMapping("Evaluator")
 	public ResponseEntity<String> addEvaluator(@RequestBody RegisterEvaluatorDTO addEvaluatorDTO) {
 		adminService.addEvaluator(addEvaluatorDTO);
 		return  ResponseEntity.status(HttpStatus.CREATED).body("Evaluator added successfully");
 	}
 
+	// Endpoint to assign evaluators to a hackathon
 	@PostMapping("assign")
 	public ResponseEntity<String> assignEvaluators(@RequestBody AddEvaluatorsDTO addEvaluatorsDTO) {
 		adminService.assignEvaluators(addEvaluatorsDTO);
 		return ResponseEntity.status(HttpStatus.OK).body("Evaluators assigned successfully");
 	}
 
+	// Endpoint to retrieve all hackathons
 	@GetMapping("hackathon")
 	public ResponseEntity<List<HackathonDTO>> getHackathons() {
 		return ResponseEntity.status(HttpStatus.OK).body(adminService.getAllHackathons());
 	}
 
+	// Endpoint to retrieve all evaluators
 	@GetMapping("Evaluator")
 	public ResponseEntity<List<GetEvaluatorsDTO>> getEvaluators() {
 		return ResponseEntity.status(HttpStatus.OK).body(adminService.getEvaluators());
 
 	}
+	
+	// Endpoint to mark a hackathon as ended
 	@PutMapping("hackathon/end/{hackathonid}")
 	public ResponseEntity<?> endHackathon(@PathVariable int hackathonid)
 	{
